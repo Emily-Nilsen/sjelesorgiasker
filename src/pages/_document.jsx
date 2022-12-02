58e8fd67f6e364f9a5ad458829400ab3dfb7aaa7
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-12-02 13:08:31 +0100

    Meta text

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className="antialiased bg-white scroll-smooth" lang="en">
+    <Html className="antialiased bg-white scroll-smooth" lang="no">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
